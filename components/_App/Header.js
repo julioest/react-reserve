f086e6b55a1d3ba8593bdd35620cbef11f9b42ea
Julio Estrada <info@julioest.com>
2020-01-12 18:26:54 -0500

    visualize route changes with progress bar

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,6 +1,12 @@
-import { Menu, Container, Image, Icon } from 'semantic-ui-react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { Menu, Container, Image, Icon } from "semantic-ui-react";
+import Link from "next/link";
+import Router, { useRouter } from "next/router";
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
+
 
 function Header() {
   const router = useRouter();

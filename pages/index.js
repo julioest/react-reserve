0f79a32353de7f375f6b3869881749773d77a14e
Julio Estrada <info@julioest.com>
2020-01-12 20:20:24 -0500

    set up getInitialProps for fetching data on server

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import axios from 'axios';
 
-function Home() {
-  React.useEffect(() => {
-    getProducts();
-  }, []);
+function Home({ products }) {
+  console.log(products);
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, []);
 
   async function getProducts() {
     const url = 'http://localhost:3000/api/products';
     const response = await axios.get(url);
-    console.log(response.data);
   }
 
   return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+  // fetch data on server
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  // return response data as an object
+  return { products: response.data };
+  // note: gets merged with existing props
 }
 
 export default Home;
